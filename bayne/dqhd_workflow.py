3d155d55a8af0e6ceef7b417bb7d8d86e52f3c0d
Brian Payne <bwpayne@gmail.com>
2025-10-12 15:41:22 -0700

    refactored using shared module

---
+++ bayne/dqhd_workflow.py
@@ -0,0 +1,277 @@
+from typing import Dict
+from typing import get_args
+from typing import List
+from typing import Literal
+
+from libqtile import bar
+from libqtile import hook
+from libqtile import layout
+from libqtile import log_utils
+from libqtile import qtile
+from libqtile import widget
+from libqtile.config import Group
+from libqtile.config import Key
+from libqtile.config import Screen
+from libqtile.group import _Group
+from libqtile.layout.base import Layout
+from libqtile.lazy import lazy
+from libqtile.lazy import LazyCall
+from libqtile.widget.base import _Widget
+
+logger = log_utils.logger
+
+MAIN_SCREEN_IDX = 0
+LEFT_SCREEN_IDX = 1
+RIGHT_SCREEN_IDX = 2
+
+mod = "mod4"
+
+WorkspaceNumberKey = Literal["1", "2", "3", "4", "5"]
+
+def sort_window(group: _Group):
+    clients = group.layout.clients
+
+    def _sort_window(window):
+        if window not in clients:
+            return -1
+        return (-clients.index(window) + clients.current_index - 1) % (len(clients))
+
+    return _sort_window
+
+class CustomTaskList(widget.TaskList):
+    @property
+    def windows(self):
+        windows = super().windows
+        windows = sorted(windows, key=sort_window(self.bar.screen.group))
+        return windows
+
+def _groups(prefix: str, screen_affinity: int) -> Dict[WorkspaceNumberKey, Group]:
+    return dict([(i, Group(name=f'{prefix}{i}', screen_affinity=screen_affinity)) for i in get_args(WorkspaceNumberKey)])
+
+class DQHDWorkflow:
+    def __init__(
+        self,
+        active_bar: str,
+        inactive_bar: str,
+        mod: str,
+    ):
+        self.active_bar = active_bar
+        self.inactive_bar = inactive_bar
+        self.mod = mod
+        self.main_groups: Dict[WorkspaceNumberKey, Group] = _groups('M', MAIN_SCREEN_IDX)
+        self.left_groups: Dict[WorkspaceNumberKey, Group] = _groups('L', LEFT_SCREEN_IDX)
+        self.right_groups: Dict[WorkspaceNumberKey, Group] = _groups('R', RIGHT_SCREEN_IDX)
+        self.last_main_group = None
+
+    def register_hooks(self,):
+        @hook.subscribe.current_screen_change
+        def on_screen_change_update_top_bar_background():
+            for screen in qtile.screens:
+                if screen.top is None:
+                    continue
+                screen.top.background = self.inactive_bar
+                for _widget in screen.top.widgets:
+                    _widget.background = self.inactive_bar
+
+            if qtile.current_screen.top is not None:
+                qtile.current_screen.top.background = self.active_bar
+                for _widget in qtile.current_screen.top.widgets:
+                    _widget.background = self.active_bar
+
+        @hook.subscribe.setgroup
+        def set_last_main_group(*_):
+            group_name = qtile.current_screen.group.name
+            if group_name in [g.name for g in self.main_groups.values()] and self.last_main_group != group_name:
+                self.last_main_group = group_name
+
+    def groups(self):
+        groups = []
+        groups.extend(self.main_groups.values())
+        groups.extend(self.left_groups.values())
+        groups.extend(self.right_groups.values())
+        return groups
+
+    def _main_screen(self, _qtile):
+        _qtile.focus_screen(MAIN_SCREEN_IDX)
+        if self.last_main_group is not None and _qtile.current_screen.group.name != self.last_main_group:
+            _qtile.screens[MAIN_SCREEN_IDX].toggle_group(self.last_main_group)
+
+    def get_group_for_current_screen(self, _qtile, number: WorkspaceNumberKey) -> _Group:
+        if _qtile.current_screen == _qtile.screens[MAIN_SCREEN_IDX]:
+            return _qtile.groups_map.get(self.main_groups[number].name)
+        elif _qtile.current_screen == _qtile.screens[LEFT_SCREEN_IDX]:
+            return _qtile.groups_map.get(self.left_groups[number].name)
+        elif _qtile.current_screen == _qtile.screens[RIGHT_SCREEN_IDX]:
+            return _qtile.groups_map.get(self.right_groups[number].name)
+        else:
+            raise ValueError(
+                f"current_screen is not in screens: {_qtile.current_screen}"
+            )
+
+    def _group_switch(self, number: WorkspaceNumberKey) -> LazyCall:
+        def _switch(_qtile):
+            _group = self.get_group_for_current_screen(_qtile, number)
+            if _group:
+                _qtile.current_screen.set_group(_group)
+        return lazy.function(_switch)
+
+    def _move_window_to_group(self, number: WorkspaceNumberKey) -> LazyCall:
+        def _move(_qtile):
+            _group = self.get_group_for_current_screen(_qtile, number)
+            if _group:
+                _qtile.current_window.togroup(_group.name)
+        return lazy.function(_move)
+
+    @staticmethod
+    def _screen_move_left(_qtile):
+        current_index = _qtile.screens.index(_qtile.current_screen)
+        if current_index == RIGHT_SCREEN_IDX:
+            _qtile.focus_screen(MAIN_SCREEN_IDX)
+        elif current_index == MAIN_SCREEN_IDX:
+            _qtile.focus_screen(LEFT_SCREEN_IDX)
+
+    @staticmethod
+    def _screen_move_window_left(_qtile):
+        current_index = _qtile.screens.index(_qtile.current_screen)
+        if current_index == RIGHT_SCREEN_IDX:
+            _qtile.current_window.toscreen(MAIN_SCREEN_IDX)
+        elif current_index == MAIN_SCREEN_IDX:
+            _qtile.current_window.toscreen(LEFT_SCREEN_IDX)
+
+    @staticmethod
+    def _screen_move_right(_qtile):
+        current_index = _qtile.screens.index(_qtile.current_screen)
+        if current_index == LEFT_SCREEN_IDX:
+            _qtile.focus_screen(MAIN_SCREEN_IDX)
+        elif current_index == MAIN_SCREEN_IDX:
+            _qtile.focus_screen(RIGHT_SCREEN_IDX)
+
+    @staticmethod
+    def _screen_move_window_right(_qtile):
+        current_index = _qtile.screens.index(_qtile.current_screen)
+        if current_index == LEFT_SCREEN_IDX:
+            _qtile.current_window.toscreen(MAIN_SCREEN_IDX)
+        elif current_index == MAIN_SCREEN_IDX:
+            _qtile.current_window.toscreen(RIGHT_SCREEN_IDX)
+
+    def keys(self) -> List[Key]:
+        keys = []
+
+        for number_key in get_args(WorkspaceNumberKey):
+            keys.extend([
+                Key(
+                    [mod],
+                    number_key,
+                    self._group_switch(number_key),
+                    desc=f"Switch to group {number_key}"
+                ),
+                Key(
+                    [mod, "shift"],
+                    number_key,
+                    self._move_window_to_group(number_key),
+                    desc=f"Switch to group {number_key}"
+                ),
+            ])
+
+        keys.append(Key([mod], "grave",
+            lazy.function(self._main_screen),
+            desc="last main group"
+        ))
+
+        # https://github.com/qtile/qtile/blob/master/libqtile/backend/x11/xkeysyms.py
+        keys.extend([
+            Key([self.mod], "h", lazy.function(self._screen_move_left), desc="Move focus to left"),
+            Key([self.mod], "l", lazy.function(self._screen_move_right), desc="Move focus to right"),
+            Key([self.mod], "j", lazy.layout.down(), desc="Move focus down"),
+            Key([self.mod], "k", lazy.layout.up(), desc="Move focus up"),
+            Key([self.mod], "Tab", lazy.layout.next(), desc="Move window focus to next window"),
+            Key([self.mod, "shift"], "Tab", lazy.layout.previous(), desc="Move window focus to prev window"),
+            Key([self.mod, "shift"], "h", lazy.function(self._screen_move_window_left), lazy.function(self._screen_move_left), desc="Move window to the left"),
+            Key([self.mod, "shift"], "l", lazy.function(self._screen_move_window_right), lazy.function(self._screen_move_right), desc="Move window to the right"),
+            Key([self.mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+            Key([self.mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+        ])
+
+        return keys
+
+    @staticmethod
+    def layouts() -> List[Layout]:
+        return [
+            layout.Max(),
+        ]
+
+    def fake_screens(self, extra_widgets: List[_Widget] = None) -> List[Screen]:
+        # 5120x1440
+        # 1280x1440+0+0, 2560x1440+1280+0, 1280x1440+3840+0
+        fake_screens: List[Screen] = []
+        fake_screens.insert(MAIN_SCREEN_IDX, Screen(
+            background="#555",
+            top=bar.Bar(
+                widgets=[
+                    widget.GroupBox(visible_groups=[g.name for g in self.main_groups.values()]),
+                    CustomTaskList(
+                        parse_text=lambda x: '',
+                        stretch=False,
+                        window_name_location=True,
+                    ),
+                    widget.WindowName(),
+                    widget.Clock(format="%a %b %d %I:%M:%S %p"),
+                    *extra_widgets,
+                    widget.TextBox(fmt="net", ),
+                    widget.NetGraph(
+                        type='line',
+                        margin_x=0,
+                        margin_y=0,
+                        border_width=0,
+                    ),
+                    widget.TextBox(fmt="cpu", ),
+                    widget.CPUGraph(
+                        type='line',
+                        margin_x=0,
+                        margin_y=0,
+                        border_width=0,
+                    ),
+                    widget.TextBox(fmt="mem", ),
+                    widget.MemoryGraph(
+                        type='line',
+                        margin_x=0,
+                        margin_y=0,
+                        border_width=0,
+                    ),
+                    widget.Systray(padding=0),
+                    widget.Spacer(length=8)
+                ],
+                size=24,
+                background=self.active_bar,
+            ),
+            x=1280, y=0, width=2560, height=1440,
+        ))
+        fake_screens.insert(LEFT_SCREEN_IDX, Screen(
+            background="#555",
+            top=bar.Bar(
+                widgets=[
+                    widget.GroupBox(visible_groups=[g.name for g in self.left_groups.values()]),
+                    CustomTaskList(
+                        parse_text=lambda x: '',
+                        stretch=False,
+                        window_name_location=True,
+                    ),
+                    widget.WindowName(),
+                ], size=24, background=self.active_bar, ),
+            x=0, y=0, width=1280, height=1440,
+        ))
+        fake_screens.insert(RIGHT_SCREEN_IDX, Screen(
+            background="#555",
+            top=bar.Bar(
+                widgets=[
+                    widget.GroupBox(visible_groups=[g.name for g in self.right_groups.values()]),
+                    CustomTaskList(
+                        parse_text=lambda x: '',
+                        stretch=False,
+                        window_name_location=True,
+                    ),
+                    widget.WindowName(),
+                ], size=24, background=self.active_bar, ),
+            x=3840, y=0, width=1280, height=1440,
+        ))
+        return fake_screens
--- config.work.py
+++ config.work.py
@@ -1,34 +1,29 @@
-import subprocess
 import os
-from typing import List, Literal, Dict, get_args
-
-from libqtile import bar
-from libqtile import hook
-from libqtile import layout
-from libqtile import log_utils
-from libqtile import qtile
-from libqtile import widget
-from libqtile.config import Click, Mouse
-from libqtile.config import Drag
-from libqtile.config import Group
-from libqtile.config import Key
-from libqtile.config import Match
-from libqtile.config import Screen
-from libqtile.group import _Group
-from libqtile.layout.base import Layout
-from libqtile.lazy import lazy, LazyCall
+import subprocess
+from typing import List
 
 from bayne import systemd_logging
-from bayne.default import get_default_floating, get_default_mouse
-from bayne.default import get_default_keys
-from bayne.default import get_default_layouts
-from bayne.default import get_default_switch_group_keys
-from bayne.default import get_widget_defaults
+from bayne.default import get_default_floating
+from bayne.default import get_default_mouse
+from bayne.dqhd_workflow import DQHDWorkflow
 from bayne.hooks import active_popup
 from bayne.hooks import disable_screensaver
 from bayne.hooks import popover
-from bayne.rofi import Rofi, RofiScript
+from bayne.rofi import Rofi
+from bayne.rofi import RofiScript
 from bayne.widgets.outlook_checker import OutlookChecker
+from dqhd_workflow import MAIN_SCREEN_IDX
+from libqtile import hook
+from libqtile import layout
+from libqtile import log_utils
+from libqtile import widget
+from libqtile.config import Group
+from libqtile.config import Key
+from libqtile.config import Match
+from libqtile.config import Mouse
+from libqtile.config import Screen
+from libqtile.layout.base import Layout
+from libqtile.lazy import lazy
 
 logger = log_utils.logger
 
@@ -40,9 +35,6 @@ popover.init(restack=[
 ])
 systemd_logging.init()
 disable_screensaver.init()
-
-BORDER_FOCUS="#CC1111"
-BORDER_NORMAL="#440000"
 
 @hook.subscribe.startup_once
 def startup():
@@ -59,12 +51,12 @@ def startup():
 ACTIVE_BAR = "#591a7d"
 INACTIVE_BAR = "#444"
 
-MAIN_SCREEN_IDX = 0
-LEFT_SCREEN_IDX = 1
-RIGHT_SCREEN_IDX = 2
+mod = "mod4"
+dqhd_workflow = DQHDWorkflow(active_bar=ACTIVE_BAR, inactive_bar=INACTIVE_BAR, mod=mod)
+dqhd_workflow.register_hooks()
+
 SHARE_SCREEN_IDX = 3
 
-mod = "mod4"
 rofi = Rofi(
     [
         RofiScript(
@@ -78,62 +70,11 @@ rofi = Rofi(
     ]
 )
 
-@hook.subscribe.current_screen_change
-def on_screen_change_update_top_bar_background():
-    for screen in qtile.screens:
-        if screen.top is None:
-            continue
-        screen.top.background = INACTIVE_BAR
-        for widget in screen.top.widgets:
-            widget.background = INACTIVE_BAR
-
-    qtile.current_screen.top.background = ACTIVE_BAR
-    for widget in qtile.current_screen.top.widgets:
-        widget.background = ACTIVE_BAR
-
-def _screen_move_left(_qtile):
-    current_index = _qtile.screens.index(_qtile.current_screen)
-    if current_index == RIGHT_SCREEN_IDX:
-        _qtile.focus_screen(MAIN_SCREEN_IDX)
-    elif current_index == MAIN_SCREEN_IDX:
-        _qtile.focus_screen(LEFT_SCREEN_IDX)
-
-def _screen_move_window_left(_qtile):
-    current_index = _qtile.screens.index(_qtile.current_screen)
-    if current_index == RIGHT_SCREEN_IDX:
-        _qtile.current_window.toscreen(MAIN_SCREEN_IDX)
-    elif current_index == MAIN_SCREEN_IDX:
-        _qtile.current_window.toscreen(LEFT_SCREEN_IDX)
-
-def _screen_move_right(_qtile):
-    logger.info("moving right")
-    current_index = _qtile.screens.index(_qtile.current_screen)
-    if current_index == LEFT_SCREEN_IDX:
-        _qtile.focus_screen(MAIN_SCREEN_IDX)
-    elif current_index == MAIN_SCREEN_IDX:
-        _qtile.focus_screen(RIGHT_SCREEN_IDX)
-
-def _screen_move_window_right(_qtile):
-    current_index = _qtile.screens.index(_qtile.current_screen)
-    if current_index == LEFT_SCREEN_IDX:
-        _qtile.current_window.toscreen(MAIN_SCREEN_IDX)
-    elif current_index == MAIN_SCREEN_IDX:
-        _qtile.current_window.toscreen(RIGHT_SCREEN_IDX)
-
-# https://github.com/qtile/qtile/blob/master/libqtile/backend/x11/xkeysyms.py
 env = os.environ.copy()
 env.update({'PATH': env['PATH'] + ':/home/bpayne/.bin'})
+# https://github.com/qtile/qtile/blob/master/libqtile/backend/x11/xkeysyms.py
 keys = [
-    Key([mod], "h", lazy.function(_screen_move_left), desc="Move focus to left"),
-    Key([mod], "l", lazy.function(_screen_move_right), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "Tab", lazy.layout.next(), desc="Move window focus to next window"),
-    Key([mod, "shift"], "Tab", lazy.layout.previous(), desc="Move window focus to prev window"),
-    Key([mod, "shift"], "h", lazy.function(_screen_move_window_left), lazy.function(_screen_move_left), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.function(_screen_move_window_right), lazy.function(_screen_move_right), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    *dqhd_workflow.keys(),
     # mod1 is alt key
     Key(["mod1", "shift"], "4", lazy.spawn('flameshot gui'), desc="screenshot"),
     # Grow windows. If current window is on the edge of screen and direction
@@ -145,85 +86,13 @@ keys = [
     Key([mod], "r", rofi.show())
 ]
 
-groups: List[Group] = []
-
-WorkspaceNumberKey = Literal["1", "2", "3", "4", "5"]
-
-main_groups: Dict[WorkspaceNumberKey, Group] = dict([(i, Group(name=f'M{i}', screen_affinity=MAIN_SCREEN_IDX)) for i in get_args(WorkspaceNumberKey)])
-groups.extend(main_groups.values())
-
-left_groups: Dict[WorkspaceNumberKey, Group] = dict([(i, Group(name=f'L{i}', screen_affinity=LEFT_SCREEN_IDX)) for i in get_args(WorkspaceNumberKey)])
-groups.extend(left_groups.values())
-
-right_groups: Dict[WorkspaceNumberKey, Group] = dict([(i, Group(name=f'R{i}', screen_affinity=RIGHT_SCREEN_IDX)) for i in get_args(WorkspaceNumberKey)])
-groups.extend(right_groups.values())
+groups: List[Group] = [
+    *dqhd_workflow.groups()
+]
 
 share_group: Group = Group(name="SH", screen_affinity=SHARE_SCREEN_IDX, layouts=[layout.Max()])
 groups.append(share_group)
 
-last_main_group = None
-
-@hook.subscribe.setgroup
-def set_last_main_group(*_):
-    global last_main_group
-    group_name = qtile.current_screen.group.name
-    if group_name in [g.name for g in main_groups.values()] and last_main_group != group_name:
-        last_main_group = group_name
-
-def _main_screen(_qtile):
-    global last_main_group
-    _qtile.focus_screen(MAIN_SCREEN_IDX)
-    if last_main_group is not None and _qtile.current_screen.group.name != last_main_group:
-        _qtile.screens[MAIN_SCREEN_IDX].toggle_group(last_main_group)
-
-def get_group_for_current_screen(_qtile, number: WorkspaceNumberKey) -> _Group:
-    if _qtile.current_screen == _qtile.screens[MAIN_SCREEN_IDX]:
-        return _qtile.groups_map.get(main_groups[number].name)
-    elif _qtile.current_screen == _qtile.screens[LEFT_SCREEN_IDX]:
-        return _qtile.groups_map.get(left_groups[number].name)
-    elif _qtile.current_screen == _qtile.screens[RIGHT_SCREEN_IDX]:
-        return _qtile.groups_map.get(right_groups[number].name)
-    else:
-        raise ValueError(
-            f"current_screen is not in screens: {_qtile.current_screen}"
-        )
-
-def group_switch(number: WorkspaceNumberKey) -> LazyCall:
-    def _switch(_qtile):
-        _group = get_group_for_current_screen(_qtile, number)
-        if _group:
-            _qtile.current_screen.set_group(_group)
-    return lazy.function(_switch)
-
-def move_window_to_group(number: WorkspaceNumberKey) -> LazyCall:
-    def _move(_qtile):
-        _group = get_group_for_current_screen(_qtile, number)
-        if _group:
-            _qtile.current_window.togroup(_group.name)
-    return lazy.function(_move)
-
-for number_key in get_args(WorkspaceNumberKey):
-    keys.extend([
-        Key(
-            [mod],
-            number_key,
-            group_switch(number_key),
-            desc=f"Switch to group {number_key}"
-        ),
-        Key(
-            [mod, "shift"],
-            number_key,
-            move_window_to_group(number_key),
-            desc=f"Switch to group {number_key}"
-        ),
-    ])
-
-keys.extend([
-    Key([mod], "grave",
-        lazy.function(_main_screen),
-        desc="last main group"
-    ),
-])
 keys.extend([
     Key([], "XF86AudioRaiseVolume", lazy.to_screen(MAIN_SCREEN_IDX), desc="move to main screen"),
     Key([], "XF86AudioLowerVolume", lazy.to_screen(SHARE_SCREEN_IDX), desc="move to share screen"),
@@ -240,105 +109,16 @@ keys.extend(
     ]
 )
 
-layouts: List[Layout] = [
-    layout.Max(),
-]
-
-widget_defaults: dict = get_widget_defaults()
-extension_defaults = widget_defaults.copy()
-
-def sort_window(group: _Group):
-    clients = group.layout.clients
-
-    def _sort_window(window):
-        if window not in clients:
-            return -1
-        return (-clients.index(window) + clients.current_index - 1) % (len(clients))
-
-    return _sort_window
-
-class CustomTaskList(widget.TaskList):
-    @property
-    def windows(self):
-        windows = super().windows
-        windows = sorted(windows, key=sort_window(self.bar.screen.group))
-        return windows
-
+layouts: List[Layout] = dqhd_workflow.layouts()
 
 # Top:5120x1440 (split into: 1280x1440,2560x1440,1280x1440)  Bottom:1920x1080 (no split)
 # 1280x1440+0+0, 2560x1440+1280+0, 1280x1440+3840+0, 1920x1080+0+1440
-fake_screens: List[Screen] = []
-fake_screens.insert(MAIN_SCREEN_IDX, Screen(
-    background="#555",
-    top=bar.Bar(
-        widgets=[
-            widget.GroupBox(visible_groups=[g.name for g in main_groups.values()]),
-            CustomTaskList(
-                parse_text=lambda x: '',
-                stretch=False,
-                window_name_location=True,
-            ),
-            widget.WindowName(),
-            widget.Clock(format="%a %b %d %I:%M:%S %p"),
-            OutlookChecker(),
-            widget.Spacer(),
-            widget.TextBox(fmt="net", ),
-            widget.NetGraph(
-                type='line',
-                margin_x=0,
-                margin_y=0,
-                border_width=0,
-            ),
-            widget.TextBox(fmt="cpu", ),
-            widget.CPUGraph(
-                type='line',
-                margin_x=0,
-                margin_y=0,
-                border_width=0,
-            ),
-            widget.TextBox(fmt="mem", ),
-            widget.MemoryGraph(
-                type='line',
-                margin_x=0,
-                margin_y=0,
-                border_width=0,
-            ),
-            widget.Systray(padding=0),
-            widget.Spacer(length=8)
-        ],
-        size=24,
-        background=ACTIVE_BAR,
-    ),
-    x=1280, y=0, width=2560, height=1440,
-))
-fake_screens.insert(LEFT_SCREEN_IDX, Screen(
-    background="#555",
-    top=bar.Bar(
-        widgets=[
-            widget.GroupBox(visible_groups=[g.name for g in left_groups.values()]),
-            CustomTaskList(
-                parse_text=lambda x: '',
-                stretch=False,
-                window_name_location=True,
-            ),
-            widget.WindowName(),
-        ], size=24, background=ACTIVE_BAR, ),
-    x=0, y=0, width=1280, height=1440,
-))
-fake_screens.insert(RIGHT_SCREEN_IDX, Screen(
-    background="#555",
-    top=bar.Bar(
-        widgets=[
-            widget.GroupBox(visible_groups=[g.name for g in right_groups.values()]),
-            CustomTaskList(
-                parse_text=lambda x: '',
-                stretch=False,
-                window_name_location=True,
-            ),
-            widget.WindowName(),
-        ], size=24, background=ACTIVE_BAR, ),
-    x=3840, y=0, width=1280, height=1440,
-))
+fake_screens: List[Screen] = [
+    *dqhd_workflow.fake_screens(extra_widgets=[
+        OutlookChecker(),
+        widget.Spacer(),
+    ]),
+]
 fake_screens.insert(SHARE_SCREEN_IDX, Screen(
     background="#333",
     x=5120, y=0, width=1920, height=1080,
